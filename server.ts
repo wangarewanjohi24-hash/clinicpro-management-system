839cfcfe55c6f3bd63c880e47c13249734769bd2
Anne Wangare <wangare.wanjohi24@students.dkut.ac.ke>
2026-04-03 14:44:07 +0300

    update server port for render deployment

--- server.ts
+++ server.ts
@@ -117,7 +117,7 @@ async function initDb() {
 async function startServer() {
   await initDb();
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT //3000;
 
   app.use(express.json());
 
